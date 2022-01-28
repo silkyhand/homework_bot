3c2b5422c1546ea414a431b1c3060ef0caffc02a
Владимир Шалдыкин <shaldykin@gmail.com>
2022-01-28 16:58:56 +0300

    from work 28.01

--- homework.py
+++ homework.py
@@ -1,11 +1,33 @@
-...
+import time
+import os
+import requests
+import telegram
+import sys
+import logging
+from logging.handlers import StreamHandler
 
+
+from dotenv import load_dotenv
+from telegram import Bot
 load_dotenv()
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='program.log', 
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+    filemode='w'
+)
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s - %(name)s - %(funcName)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,14 +42,27 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
-    ...
+    """ Отправляем сообщение в Telegram чат"""
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        logger.info(f'Сообщение {message} отправлено в {TELEGRAM_CHAT_ID}')
+    except Exception:
+        logger.error(f'Сообщение {message} не отправлено в {TELEGRAM_CHAT_ID}')
+
 
 
 def get_api_answer(current_timestamp):
+    """Запрос к эндпоинту API-сервиса яндекса.    
+    возвращает ответ API, преобразованный из JSON в Python
+    """
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    try:
+        homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        raise Exception(f'Ошибка при запросе к основному API: {error}')    
+    
 
 
 def check_response(response):
@@ -53,14 +88,10 @@ def check_tokens():
 
 
 def main():
-    """Основная логика работы бота."""
-
-    ...
-
+    """Основная логика работы бота."""    
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
 
-    ...
 
     while True:
         try:
